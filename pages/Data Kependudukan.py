6d077e7ef1af506f99e5e2f4cde39068dd98addc
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-07 08:34:44 +0700

    Update Data Kependudukan.py

--- pages/Data Kependudukan.py
+++ pages/Data Kependudukan.py
@@ -2,82 +2,65 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
+st.set_page_config(
+    page_title="Data Kependudukan",
+    page_icon=":family:",
+)
+
+#this is the header
+t1, t2 = st.columns((0.25,1))
+
+t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon-logo-dewi-warna-600x721.png', width = 120)
+t2.title("Desa Cibiru Wetan")
+t2.write("**Data Kependudukan**")
+t2.markdown(" **website:** https://cibiruwetan.desa.id **| email:** desawisatacibiruwetan@gmail.com")
+
+
+#this is content
+ #st.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2023/01/branding-cibiru-wetan-WISATA-1-800x197.png')
+st.write("# Piramida Penduduk Tahun 2024")
+
 ### Import Data Lengkap
-url = 'https://docs.google.com/spreadsheets/d/10TvMMPQnOEKG8gABZZw2LXAK1XDHsU17oGsXkhiORdA/edit?gid=0#gid=0'
+url2='https://docs.google.com/spreadsheets/d/16AtuoSRO-7SwU8E6jJDNzdoX6S0DyRFkpaduxBhZ748/edit?usp=sharing'
 conn  = st.experimental_connection("gsheets", type=GSheetsConnection)
-datadesa = conn.read(spreadsheet=url)
-#dataumkm = pd.read_excel("C:/Users/wella/Documents/Asha/SMT7/magang/DESA SEKARWANGI/Data UMKM Sekarwangi_ready.xlsx")
-type(datadesa)
-datadesa["NIK"] = datadesa["NIK"].astype("string")
-datadesa["No HP"] = datadesa["No HP"].astype("string")
-datadesa['ALAMAT'] = datadesa['ALAMAT'].str.rstrip()
-datadesa['RW'] = datadesa['ALAMAT'].str[-5:]
-datadesa['RT'] = datadesa['ALAMAT'].str[-11:]
-df = datadesa
+datap2024 = conn.read(spreadsheet=url2)
+datap2024 = pd.DataFrame(datap2024)                       #convert ke panda df
+jp2024=datap2024.iloc[0:16,1:3].sum().sum()
 
-### Filter Data
-from pandas.api.types import(
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype
+datapiramida = datap2024.iloc[0:16,1:3]
+jk = list(["Laki-laki","Perempuan"])
+datapiramida.iloc[0:16,0]=-datapiramida.iloc[0:16,0]
+datapiramida.index = list(datap2024.iloc[0:16,0])
+datapiramida.columns = jk
+#st.bar_chart(datapiramida)
+
+import altair as alt
+# Convert wide-form data to long-form
+# See: https://altair-viz.github.io/user_guide/data.html#long-form-vs-wide-form-data
+data = pd.melt(datapiramida.reset_index(), id_vars=["index"])
+
+# Horizontal stacked bar chart
+chart = (
+    alt.Chart(data)
+    .mark_bar()
+    .encode(
+        x=alt.X("value", type="quantitative", title=""),
+        y=alt.Y("index", type="nominal", title="",sort="descending"),
+        color=alt.Color("variable", type="nominal", title=""),
     )
+)
 
-st.subheader("Data Lengkap UMKM Desa Sekarwangi")
-st.write("Pilih kolom yang ingin ditampilkan")
+st.altair_chart(chart, use_container_width=True)   #bikin piramida chart
 
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Menambahkan UI di atas dataframe untuk memungkinkan pengguna melakukan filter kolom
-
-    Args:
-        df(pd.DataFrame): Original dataframe
-
-    Returns:
-        pd.DataFrame: Filtered dataframe
-
-    Sumber: https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/
-    """
-    modify = st.checkbox("Tambahkan filter")
-    if not modify:
-        return df
-    df = df.copy()
-
-    modification_container = st.container()
-
-    with modification_container:
-        to_filter_columns = st.multiselect("Filter data berdasarkan", dataumkm.columns)
-        for column in to_filter_columns:
-            left, right = st.columns((1,20))
-            left.write("↳")
-            #Treat columns with < 10 unique values a s categorical
-            if is_numeric_dtype(df[column]):
-                st.write(f'Values for {column}')
-                _min = st.text_input('Rentang Terkecil', float(df[column].min()))
-                _max = st.text_input('Rentang Tertinggi', float(df[column].max()))
-                _min = float(_min)
-                _max = float(_max)
-                df= df[df[column].between(_min,_max)]
-            else:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                    )
-                df = df[df[column].isin(user_cat_input)]
-        return df
-filtered_df = filter_dataframe(df)
-
+st.dataframe(datap2024)   #menampilkan data
 ### Opsi Download Data
 @st.cache_data
-def convert_df(filtered_df):
-    return filtered_df.to_csv().encode('utf-8')
-csv = convert_df(filtered_df)
+def convert_df(datap2024):
+    return datap2024.to_csv().encode('utf-8')
+csv = convert_df(datap2024)
 st.download_button(
     label = "Unduh Data",
     data = csv,
     file_name='download_sekarwangi.csv',
     mime='text/csv',
     )
-
-st.dataframe(filtered_df)   #menampilkan hasil filter
