ff00e57bc4bc288bd789008b38eb43290fd1e741
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-08 22:18:26 +0700

    Update Data Kependudukan.py

--- pages/Data Kependudukan.py
+++ pages/Data Kependudukan.py
@@ -17,9 +17,9 @@ t2.markdown(" **website:** https://cibiruwetan.desa.id **| email:** desawisataci
 
 
 #this is content
- #st.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2023/01/branding-cibiru-wetan-WISATA-1-800x197.png')
-st.write("# Piramida Penduduk Tahun 2024")
+st.write("# Struktur Penduduk Tahun 2024")
 
+##data penduduk
 ### Import Data Lengkap
 url2='https://docs.google.com/spreadsheets/d/16AtuoSRO-7SwU8E6jJDNzdoX6S0DyRFkpaduxBhZ748/edit?usp=sharing'
 conn  = st.connection("gsheets", type=GSheetsConnection)
@@ -65,8 +65,8 @@ st.download_button(
     mime='text/csv',
     )
 
+st.write("# Struktur Penduduk Tahun 2023")
 url3 = 'https://docs.google.com/spreadsheets/d/196eZm3CzaY1FIapq0nnWBc18dFz0yXxtPrxtlYaqRLc/edit?usp=sharing'
-conn  = st.connection("gsheets", type=GSheetsConnection)
 datap2023 = conn.read(spreadsheet=url3)
 datap2023 = pd.DataFrame(datap2023)                       #convert ke panda df
 jp2023=datap2023.iloc[0:16,1:3].sum().sum()
@@ -95,3 +95,51 @@ chart2 = (
 st.altair_chart(chart2, use_container_width=True)   #bikin piramida chart
 
 st.dataframe(datap2023.iloc[0:16,0:3], use_container_width=True, hide_index=True)   #menampilkan data
+
+#data pekerjaan
+url4 = 'https://docs.google.com/spreadsheets/d/1wjTtBvxU2a-x0LGEfkX7IaQcN1DhkzJ4ZqRk1DYO8Yw/edit?usp=sharing'
+kerja23 = conn.read(spreadsheet=url4)
+kerja23 = pd.DataFrame(kerja23)
+stkerja23 = kerja23.iloc[16:18,0:4]
+kerja23 = kerja23.iloc[0:13,0:4]
+
+st.write('# Status Pekerjaan Angkatan Kerja Tahun 2023')
+stkerja23.index = list(stkerja23.iloc[0:2,0])
+stkerja23 = stkerja23.iloc[0:2,1:4]
+st.dataframe(stkerja23,use_container_width=True)
+
+st.write('# Pekerjaan Penduduk Tahun 2023')
+kerja23.index = list(kerja23.iloc[0:13,0])
+kerja23 = kerja23.iloc[0:13,1:4]
+#st.bar_chart(kerja23.iloc[0:13,0].T)
+st.dataframe(kerja23,use_container_width=True)
+
+st.write('# Pendidikan Tenaga Kerja Tahun 2023')
+url5 = 'https://docs.google.com/spreadsheets/d/1BaFB0sFvKIv_e3G8LX_6su-vwDUJKhZLWeE1knU5bT8/edit?usp=sharing'
+pdik23 = conn.read(spreadsheet=url5)
+pdik23 = pd.DataFrame(pdik23)
+pdik23 = pdik23.iloc[0:6,0:3]
+pdik23.index = list(pdik23.iloc[0:6,0])
+pdik23 = pdik23.iloc[0:6,1:3]
+st.dataframe(pdik23,use_container_width=True)
+
+st.write('# Etnis Penduduk Tahun 2023')
+url6 = 'https://docs.google.com/spreadsheets/d/1lrJgC7IgzaYUElTEzhix6m2Wz-u-ThN1WXsGRHgMv8s/edit?usp=sharing'
+et23 = conn.read(spreadsheet=url6)
+et23 = pd.DataFrame(et23)
+et23.index = list(et23.iloc[0:21,0])
+et23 = et23.iloc[0:21,1:3]
+st.dataframe(et23,use_container_width=True)
+
+st.write('# Agama Penduduk Tahun 2023')
+url7 = 'https://docs.google.com/spreadsheets/d/1dUtknRWJL7X_5qOs0zLF4zbFH2B4qPjyjkI6SMUxzwY/edit?usp=sharing'
+agam23 = conn.read(spreadsheet=url7)
+agam23 = pd.DataFrame(agam23)
+agam23.index = list(agam23.iloc[0:9,0])
+agam23 = agam23.iloc[0:8,1:4]
+st.dataframe(agam23,use_container_width=True)
+
+with st.sidebar:
+    st.image('https://www.bpskotabaru.com/desacantik/public/images/Logo%20DESCAN_1_002.png',width=100)
+    st.header("Dashboard Data Penduduk Desa Cibiru Wetan")
+    st.caption("""Data kependudukan ini menyediakan data jumlah penduduk berdasarkan usia, jenis kelamin, serta data tingkat pendidikan dan status pendidikan angkatan kerja (penduduk usia 18-56 tahun) di Desa Cibiru Wetan, Kecamatan Cileunyi, Kabupaten Bandung, Jawa Barat.""")
