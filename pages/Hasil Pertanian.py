c1aa387da164459bb72b1e3bbb55a72177210b4f
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-08 22:51:43 +0700

    Update Hasil Pertanian.py

--- pages/Hasil Pertanian.py
+++ pages/Hasil Pertanian.py
@@ -28,17 +28,17 @@ pt23 = pt23.iloc[0:65,0:3]
 #st.write(sizes)
 #st.write(label)
 
-#label = list((lahan23.index))
-#sizes = list(lahan23.iloc[0:3,0])
+labels = list((lahan23.index))
+sizes = list(lahan23.iloc[0:3,0])
 import matplotlib.pyplot as plt
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+#labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+#sizes = [15, 30, 45, 10]
+#explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
