04226613722a65233bdbacfa7e690fcaa426f524
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-08 22:49:04 +0700

    Update Hasil Pertanian.py

--- pages/Hasil Pertanian.py
+++ pages/Hasil Pertanian.py
@@ -28,17 +28,21 @@ pt23 = pt23.iloc[0:65,0:3]
 st.write(sizes)
 st.write(label)
 
-import matplotlib.pyplot as plt
 #label = list((lahan23.index))
 #sizes = list(lahan23.iloc[0:3,0])
+import matplotlib.pyplot as plt
 
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 sizes = [15, 30, 45, 10]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels)
-st.pyplot(fig)
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
+st.pyplot(fig1)
 #st.bar_chart(lahan23)
 
 
