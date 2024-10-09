639f1ded531eb1712274d86f20bbc30d400d9039
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-09 08:02:54 +0700

    Update Fasilitas Umum.py

--- pages/Fasilitas Umum.py
+++ pages/Fasilitas Umum.py
@@ -10,10 +10,10 @@ st.set_page_config(
 #this is the header
 t1, t2 = st.columns((0.25,1))
 
-t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon-logo-dewi-warna-600x721.png', width = 120)
+t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon-logo-dewi-warna-600x721.png', width = 100)
 t2.title("Desa Cibiru Wetan")
 t2.write("**Fasilitas Umum**")
-#t2.markdown(" **website:** https://cibiruwetan.desa.id **| email:** desawisatacibiruwetan@gmail.com")
+t2.markdown(" **Halaman Data Fasilitas Umum Desa Cibiru Wetan, Cileunyi, Kab. Bandung** ")
 
 url9='https://docs.google.com/spreadsheets/d/1Skt6QdDL1_EKQJ3MgdJG53-FtCevRv56pRZNOyBf4lI/edit?usp=sharing'
 conn  = st.connection("gsheets", type=GSheetsConnection)
