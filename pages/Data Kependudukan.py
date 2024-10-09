5e773c90bf4fdf13d8792171a5962679834db398
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-09 08:05:34 +0700

    Update Data Kependudukan.py

--- pages/Data Kependudukan.py
+++ pages/Data Kependudukan.py
@@ -12,7 +12,6 @@ t1, t2 = st.columns((0.25,1))
 
 t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon-logo-dewi-warna-600x721.png', width = 100)
 t2.title("Desa Cibiru Wetan")
-t2.write("**Data Kependudukan**")
 t2.markdown(" **Halaman Data Kependudukan Desa Cibiru Wetan, Cileunyi, Kab. Bandung** ")
 
 
