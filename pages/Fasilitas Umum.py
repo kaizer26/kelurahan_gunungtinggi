05e1f7e29dd49f5a7ef0328f778246d37cb24ccc
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-10 06:45:06 +0700

    Update Fasilitas Umum.py

--- pages/Fasilitas Umum.py
+++ pages/Fasilitas Umum.py
@@ -23,8 +23,8 @@ fas23 = fas23.iloc[1:94,0:3]
 
 #this is content
 st.write("# Fasilitas Pendidikan")
+
 s_ag = st.checkbox("termasuk sekolah keagamaan",value=True)           #dengan sekolah agama
-st.write(s_ag)
 
 if s_ag:
     tka=fas23.iloc[13,1]
