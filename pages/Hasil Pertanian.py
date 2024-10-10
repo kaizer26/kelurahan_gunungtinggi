ea0baebec0c3b04daed057c8f8b629ce29270636
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-10 09:23:02 +0700

    Update Hasil Pertanian.py

--- pages/Hasil Pertanian.py
+++ pages/Hasil Pertanian.py
@@ -12,8 +12,8 @@ t1, t2 = st.columns((0.25,1))
 
 t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon-logo-dewi-warna-600x721.png', width = 100)
 t2.title("Desa Cibiru Wetan")
-t2.markdown(" **Halaman Data Hasil Pertanian dan Perkebunan Desa Cibiru Wetan, Cileunyi, Kab. Bandung** ")
-st.write(" **Tahun 2023** ")
+t2.markdown(" **Halaman Data Hasil Pertanian dan Perkebunan Desa Cibiru Wetan** ")
+t2.write(" **Tahun 2023** ")
 
 url10='https://docs.google.com/spreadsheets/d/15sfFQbVZUmseAfOv70EXI4pe3wUusL9OvfeOex43ueg/edit?usp=sharing'
 conn  = st.connection("gsheets", type=GSheetsConnection)
@@ -38,20 +38,38 @@ chart = (
         color=alt.Color("variable", type="nominal", title=""),
     )
 )
-st.altair_chart(chart, use_container_width=True)   
-
-chart = alt.Chart(data).mark_arc().encode(
+text = chart.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='value'
+)
+chart = (chart + text)
+st.altair_chart(chart, use_container_width=True)
+datapie = data
+datapie['value'] = datapie['value'].div(float(data['value'].sum().sum()/100)).round(2)
+piechart = alt.Chart(datapie,title=alt.TitleParams('Persentase (%) Luas Lahan Pertanian Berdasarkan Jenis Produksi', anchor='middle')).mark_arc().encode(
     theta=alt.Theta(field="value", type="quantitative"),
     color=alt.Color(field="index", type="nominal"),
 )
-st.altair_chart(chart, use_container_width=True)  
+piechart = piechart.mark_arc(outerRadius=120)
+textpie = piechart.mark_text(radius=140, size=20).encode(text="value")
+
+piechart = piechart + textpie
+tampil = st.checkbox("Tampilkan Grafik dalam Persentase")
+if tampil:
+    st.altair_chart(piechart, use_container_width=True)
 
 st.write('## Tanaman Pangan Berdasarkan Komoditas')
-datatp1 = pt23.iloc[0:31,0]
+pilih1 = st.radio("Menampilkan:",['Luas Lahan (Ha)','Hasil Panen (Ton/Ha)'],key="pangan")
+if pilih1 =='Luas Lahan (Ha)':
+    datatp1 = pt23.iloc[0:31,0]
+elif pilih1 == 'Hasil Panen (Ton/Ha)':
+    datatp1 = pt23.iloc[0:31,1]
 datatp1 = pd.melt(datatp1.reset_index(), id_vars=["index"])
-# Horizontal stacked bar chart
 charttp1 = (
-    alt.Chart(datatp1,title=alt.TitleParams('Luas Lahan (Ha)', anchor='middle'))
+    alt.Chart(datatp1,title=alt.TitleParams(pilih1, anchor='middle'))
     .mark_bar()
     .encode(
         x=alt.X("value", type="quantitative", title=""),
@@ -67,56 +85,18 @@ text1 = charttp1.mark_text(
     text='value'
 )
 charttp1 = (charttp1 + text1)
-datatp2 = pt23.iloc[0:31,1]
-datatp2 = pd.melt(datatp2.reset_index(), id_vars=["index"])
-# Horizontal stacked bar chart
-charttp2 = (
-    alt.Chart(datatp2,title=alt.TitleParams('Hasil Panen (Ton/Ha)', anchor='middle'))
-    .mark_bar()
-    .encode(
-        x=alt.X("value", type="quantitative", title=""),
-        y=alt.Y("index", type="nominal", title="",sort="descending"),
-        color=alt.Color("variable", type="nominal", title="",legend=None),
-    )
-)
-text2 = charttp2.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='value'
-)
-charttp2 = (charttp2 + text2)
-tp1,tp2 = st.columns((1,1))
-tp1.altair_chart(charttp1)   
-tp2.altair_chart(charttp2)   
+st.altair_chart(charttp1,use_container_width=True)
 
 st.write('## Tanaman Buah-buahan Berdasarkan Komoditas')
-datatp1 = pt23.iloc[31:58,0]
-datatp1 = pd.melt(datatp1.reset_index(), id_vars=["index"])
-# Horizontal stacked bar chart
-charttp1 = (
-    alt.Chart(datatp1,title=alt.TitleParams('Luas Lahan (Ha)', anchor='middle'))
-    .mark_bar()
-    .encode(
-        x=alt.X("value", type="quantitative", title=""),
-        y=alt.Y("index", type="nominal", title="",sort="descending"),
-        color=alt.Color("variable", type="nominal", title="",legend=None),
-    )
-)
-text1 = charttp1.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='value'
-)
-charttp1 = (charttp1 + text1)
-datatp2 = pt23.iloc[31:58,1]
+pilih2 = st.radio("Menampilkan:",['Luas Lahan (Ha)','Hasil Panen (Ton/Ha)'],key="buah")
+if pilih2 =='Luas Lahan (Ha)':
+    datatp2 = pt23.iloc[31:58,0]
+elif pilih2 == 'Hasil Panen (Ton/Ha)':
+    datatp2 = pt23.iloc[31:58,1]
 datatp2 = pd.melt(datatp2.reset_index(), id_vars=["index"])
 # Horizontal stacked bar chart
 charttp2 = (
-    alt.Chart(datatp2,title=alt.TitleParams('Hasil Panen (Ton/Ha)', anchor='middle'))
+    alt.Chart(datatp2,title=alt.TitleParams(pilih2, anchor='middle'))
     .mark_bar()
     .encode(
         x=alt.X("value", type="quantitative", title=""),
@@ -132,16 +112,17 @@ text2 = charttp2.mark_text(
     text='value'
 )
 charttp2 = (charttp2 + text2)
-tp1,tp2 = st.columns((1,1))
-tp1.altair_chart(charttp1)   
-tp2.altair_chart(charttp2)  
+st.altair_chart(charttp2,use_container_width=True)
 
 st.write('## Perkebunan Berdasarkan Komoditas')
-datatp1 = pt23.iloc[58:65,0]
-datatp1 = pd.melt(datatp1.reset_index(), id_vars=["index"])
-# Horizontal stacked bar chart
-charttp1 = (
-    alt.Chart(datatp1,title=alt.TitleParams('Luas Lahan (Ha)', anchor='middle'))
+pilih3 = st.radio("Menampilkan:",['Luas Lahan (Ha)','Hasil Panen (Ton/Ha)'],key="kebun")
+if pilih3 =='Luas Lahan (Ha)':
+    datatp3 = pt23.iloc[58:65,0]
+elif pilih3 == 'Hasil Panen (Ton/Ha)':
+    datatp3 = pt23.iloc[58:65,1]
+datatp3 = pd.melt(datatp3.reset_index(), id_vars=["index"])
+charttp3 = (
+    alt.Chart(datatp3,title=alt.TitleParams(pilih3, anchor='middle'))
     .mark_bar()
     .encode(
         x=alt.X("value", type="quantitative", title=""),
@@ -149,37 +130,15 @@ charttp1 = (
         color=alt.Color("variable", type="nominal", title="",legend=None),
     )
 )
-text1 = charttp1.mark_text(
+text3 = charttp3.mark_text(
     align='left',
     baseline='middle',
     dx=3  # Nudges text to right so it doesn't appear on top of the bar
 ).encode(
     text='value'
 )
-charttp1 = (charttp1 + text1)
-datatp2 = pt23.iloc[58:65,1]
-datatp2 = pd.melt(datatp2.reset_index(), id_vars=["index"])
-# Horizontal stacked bar chart
-charttp2 = (
-    alt.Chart(datatp2,title=alt.TitleParams('Hasil Panen (Ton/Ha)', anchor='middle'))
-    .mark_bar()
-    .encode(
-        x=alt.X("value", type="quantitative", title=""),
-        y=alt.Y("index", type="nominal", title="",sort="descending"),
-        color=alt.Color("variable", type="nominal", title="",legend=None),
-    )
-)
-text2 = charttp2.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='value'
-)
-charttp2 = (charttp2 + text2)
-tp1,tp2 = st.columns((1,1))
-tp1.altair_chart(charttp1)   
-tp2.altair_chart(charttp2)  
+charttp3 = (charttp3 + text3)
+st.altair_chart(charttp3,use_container_width=True)
 
 st.write('# Tabel Luas Lahan Hasil Pertanian dan Perkebunan')
 st.dataframe(pt23,use_container_width=True)
