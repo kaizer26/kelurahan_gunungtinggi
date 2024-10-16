60e5bd2cfab17aefbc718d77e0073959cb78c2c6
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-16 10:06:30 +0700

    Update Unduh Publikasi.py

--- pages/Unduh Publikasi.py
+++ pages/Unduh Publikasi.py
@@ -18,16 +18,17 @@ t1.image('https://www.desawisata-cibiruwetan.com/wp-content/uploads/2024/09/icon
 t2.title("Desa Cantik Cibiru Wetan")
 t2.markdown(" **Halaman Unduh Publikasi Desa Cibiru Wetan** ")
 
-urlmono23 = 'https://drive.google.com/file/d/1ybr7FUyu74dCiogJKLjgvmKoRFDpYf69/view?usp=drive_link'
-urlstat23 = 'https://drive.google.com/file/d/1AfqhgA_YRcW17ARNogD2Mzl6mWtIjB6h/view?usp=drive_link'
+urlmono23 = 'https://drive.google.com/file/d/1wqu3XjSCo_ro7pV_xgxJF5WNlBYAW3rb/view?usp=sharing'
+urlstat23 = 'https://drive.google.com/file/d/1hTDGnTyuNZt0_n6CyZXk4ZzO6yKyaMSs/view?usp=sharing'
+urlprof24 = 'https://drive.google.com/file/d/1T75Q0mMxDhrK234yPxjvSKsHexCCjQuC/view?usp=sharing'
 
 pd1,pd2,pd3 = st.columns((1,1,1))
-pd1.image('cover_publikasi/monografi2023.png')
-pd1.write("[Unduh](%s)"%urlmono23)
-pd2.image('cover_publikasi/statistik2023.png')
-pd2.write("[Unduh](%s)"%urlstat23)
-pd3.image('cover_publikasi/statistik2022.png')
-pd3.write("Unduh")
+pd1.image('cover_publikasi/profil2024.png')
+pd1.write("[Unduh](%s)"urlprof24)
+pd2.image('cover_publikasi/monografi2023.png')
+pd2.write("[Unduh](%s)"%urlmono23)
+pd3.image('cover_publikasi/statistik2023.png')
+pd3.write("[Unduh](%s)"%urlstat23)
 
 with st.sidebar:
     st.image('https://www.bpskotabaru.com/desacantik/public/images/Logo%20DESCAN_1_002.png',width=100)
