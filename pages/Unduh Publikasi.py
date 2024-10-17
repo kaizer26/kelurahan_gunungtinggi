9d3198cc51dc3c6eb243eed49214207c4660e8ae
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-17 08:40:10 +0700

    Update Unduh Publikasi.py

--- pages/Unduh Publikasi.py
+++ pages/Unduh Publikasi.py
@@ -53,6 +53,6 @@ while i < len(publikasi):
     i += 3
     
 with st.sidebar:
-    st.image('https://www.bpskotabaru.com/desacantik/public/images/Logo%20DESCAN_1_002.png',width=100)
+    st.image('desa_cantik.png',width=100)
     st.header("Dashboard Unduh Publikasi Desa Cibiru Wetan")
     st.caption("""Menu Unduh Publikasi menyediakan publikasi yang berisikan kompilasi informasi dan data di Desa Cibiru Wetan, Kecamatan Cileunyi, Kabupaten Bandung, Jawa Barat.""")
