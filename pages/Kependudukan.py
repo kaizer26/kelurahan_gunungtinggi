52ed72a501d1ad36e4c092d5b45bf3d1a66210f9
markskjaer <143985997+markskjaer@users.noreply.github.com>
2024-10-17 08:39:28 +0700

    Update Kependudukan.py

--- pages/Kependudukan.py
+++ pages/Kependudukan.py
@@ -273,6 +273,6 @@ with st.expander("lihat tabel"):
     st.dataframe(agam23,use_container_width=True)
 
 with st.sidebar:
-    st.image('https://www.bpskotabaru.com/desacantik/public/images/Logo%20DESCAN_1_002.png',width=100)
+    st.image('desa_cantik.png',width=100)
     st.header("Dashboard Data Kependudukan Desa Cibiru Wetan")
     st.caption("""Menu kependudukan ini menyediakan data jumlah penduduk berdasarkan usia, jenis kelamin, serta data tingkat pendidikan dan status pendidikan angkatan kerja (penduduk usia 18-56 tahun) di Desa Cibiru Wetan, Kecamatan Cileunyi, Kabupaten Bandung, Jawa Barat.""")
